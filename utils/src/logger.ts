77355d6c2f2000884504feef5b0e3bc65d4e2218
Tomas Vargas <vieira.tomasvargas@gmail.com>
2025-03-18 15:32:29 -0300

    docs(utils): documenta todas as classes e funções utilitárias

--- utils/src/logger.ts
+++ utils/src/logger.ts
@@ -1,17 +1,52 @@
 // @ts-ignore
 import chalk from "chalk";
 
+/**
+ * Classe utilitária para imprimir mensagens no console com diferentes níveis de log.
+ *
+ * Níveis de log disponíveis:
+ * - INFO: Mensagens de informação geral.
+ * - ERROR: Mensagens de erro, com opção de informações de erro adicionais.
+ * - DEBUG: Mensagens de debug, com opção de informações de debug adicionais.
+ * - WARNING: Mensagens de aviso.
+ *
+ * Examplos de uso::
+ * ```typescript
+ * Logger.info("Aplicação iniciada.");
+ * Logger.error("Ocorreu um erro", new Error("Exemplo de erro"));
+ * Logger.debug("Debugando", { key: "value" });
+ * Logger.warning("Atenção");
+ * ```
+ */
 class Logger {
+
+	/**
+	 * Imprime uma mensagem no console com data/hora, o nível do log e uma mensagem definida.
+	 *
+	 * @param level - O nível do log (exemplo: "INFO", "ERROR"), que indica o tipo do log ou sua gravidade.
+	 * @param message - A mensagem a ser impressa.
+	 */
 	private static logWithDate(level: string, message: string): void {
 		const date = new Date().toLocaleString();
 
 		console.log(`${chalk.green(date)} ${level} ${message}`);
 	}
 
+	/**
+	 * Chama a função {@link logWithDate()} com o nível "INFO", imprimindo no console informações, uma mensagem e a data/hora.
+	 *
+	 * @param message - A mensagem a ser impressa.
+	 */
 	public static info(message: string): void {
 		this.logWithDate(chalk.cyan("[INFO]"), `${message}`);
 	}
 
+	/**
+	 * Chama a função {@link logWithDate()} com o nível "ERROR", imprimindo no console uma mensagem com data/hora, e opcionalmente os detalhes do erro.
+	 *
+	 * @param message - A mensagem a ser impressa.
+	 * @param err - Parâmetro opcional. Um objeto "Error", contendo detalhes adicionais do erro.
+	 */
 	public static error(message: string, err?: Error): void {
 		this.logWithDate(chalk.red("[ERROR]"), `${message}`);
 
@@ -20,6 +55,12 @@ class Logger {
 		}
 	}
 
+	/**
+	 * Chama a função {@link logWithDate()} com o nível "DEBUG", imprimindo no console uma mensagem com data/hora, e opcionalmente um objeto para contexto adicional.
+	 *
+	 * @param message - A mensagem a ser impressa.
+	 * @param object - Parâmetro opcional, Um objeto que, se providenciado, serve para imprimir informações adicionais no console.
+	 */
 	public static debug(message: string, object?: any): void {
 		this.logWithDate(chalk.blue("[DEBUG]"), `${message}`);
 
@@ -28,6 +69,11 @@ class Logger {
 		}
 	}
 
+	/**
+	 * Chama a função {@link logWithDate()} com o nível "WARNING", imprimindo no console uma mensagem com data/hora.
+	 *
+	 * @param message - A mensagem a ser impressa.
+	 */
 	public static warning(message: string): void {
 		this.logWithDate(chalk.yellow("[WARNING]"), `${message}`);
 	}
--- utils/src/task-queue.ts
+++ utils/src/task-queue.ts
@@ -1,9 +1,45 @@
+/**
+ * As opções para se configurar a task queue.
+ *
+ * @property concurrency - O número máximo de "tasks" que podem ser processadas simultaneamente.
+ * @property onTaskCompleted - Callback opcional invocada quando uma tarefa é concluída com sucesso. Recebe o resultado da task concluída como argumento.
+ * @property onTaskFailed - Callback opcional invocada quando uma tarefa falha. Recebe o erro lançado pela task falhada como argumento.
+ */
 interface TaskQueueOptions<T> {
 	concurrency: number;
 	onTaskCompleted?: (result: T) => void;
 	onTaskFailed?: (error: unknown) => void;
 }
 
+/**
+ * Uma fila de tarefas ("tasks") genérica que administra a execução de tarefas assíncronas com um limite de simultaneidade especificado.
+ *
+ * @remarks
+ * - As tarefas são executadas na ordem em que são adicionadas à fila.
+ * - O número de tarefas sendo executadas simultaneamente é limitado pelo valor de simultaneidade ("concurrency").
+ * - Callbacks opcionais podem ser providenciados para lidar com tarefas finalizadas ou em caso de erro.
+ *
+ * @example
+ * ```typescript
+ * const queue = new TaskQueue<string>({
+ *   concurrency: 2,
+ *   onTaskCompleted: (resultado) => console.log(`Tarefa concluída com o resultado: ${resultado}`),
+ *   onTaskFailed: (erro) => console.error(`Tarefa falhou com o erro: ${erro}`),
+ * });
+ *
+ * queue.add(async () => {
+ *   await new Promise((resolve) => setTimeout(resolve, 1000));
+ *   return "Task 1";
+ * });
+ *
+ * queue.add(async () => {
+ *   await new Promise((resolve) => setTimeout(resolve, 500));
+ *   return "Task 2";
+ * });
+ *
+ * queue.completion().then(() => console.log("Todas as tarefas foram finalizadas!."));
+ * ```
+ */
 class TaskQueue<T> {
 	private readonly queue: Array<() => Promise<T>> = [];
 	private readonly running: Array<Promise<void>> = [];
@@ -17,6 +53,17 @@ class TaskQueue<T> {
 		this.onTaskFailed = options.onTaskFailed;
 	}
 
+	/**
+	 * Executa uma tarefa assíncrona e trata sua conclusão, falha, e tarefas subsequentes na fila.
+	 *
+	 * @param task - Uma função que retorna uma promessa atribuida a um valor de tipo genérico "T".
+	 * @returns Uma promessa que é resolvida quando a "task" e quaisquer "tasks" subsequentes na fila são completadas.
+	 *
+	 * @remarks
+	 * - Se a "task" finalizar com sucesso, a callback "onTaskCompleted" é chamada com o resultado.
+	 * - Se a "task" falhar, a callback "onTaskFailed" é chamada com o erro, se definido.
+	 * - Depois da "task" ser processada, a próxima "task" na fila (se houver) é executada recursivamente.
+	 */
 	private async run(task: () => Promise<T>) {
 		try {
 			const data = await task();
@@ -37,6 +84,13 @@ class TaskQueue<T> {
 		}
 	}
 
+	/**
+	 * Adiciona uma "task" à fila de "tasks". Se o número de "tasks" atualmente em 
+	 * execução for menor do que o permitido pelo valor de simultaneidade (concurrency),
+	 * a "task" é executada imediatamente. Caso contrário, a "task" é adicionada à fila para ser executada depois.
+	 *
+	 * @param task - Uma função que retorna uma promessa de tipo genérico "T", representando a "task" assíncrona a ser executada.
+	 */
 	public add(task: () => Promise<T>) {
 		if (this.running.length < this.concurrency) {
 			this.running.push(this.run(task));
@@ -45,6 +99,11 @@ class TaskQueue<T> {
 		}
 	}
 
+	/**
+	 * Espera para que todas as "tasks" atualmente sendo executadas finalizarem.
+	 *
+	 * @returns Uma promessa que é finalizada quando todas as "tasks" na fila de nome "running" terminam de executar.
+	 */
 	public completion() {
 		return Promise.all(this.running);
 	}
