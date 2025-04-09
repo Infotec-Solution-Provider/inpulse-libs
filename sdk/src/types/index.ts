2b97851da5ea52ce91dcf5ccb7e6194f194e2e37
Renan G Dutra <r.granatodutra@gmail.com>
2025-04-09 12:41:32 -0300

    chore: add reports to exports and organize imports

--- sdk/src/types/index.ts
+++ sdk/src/types/index.ts
@@ -1,7 +1,9 @@
+export * from "./auth.types";
+export * from "./customers.types";
+export * from "./files.types";
+export * from "./reports.types";
 export * from "./response.types";
 export * from "./socket-events.types";
 export * from "./socket-rooms.types";
-export * from "./auth.types";
 export * from "./user.types";
-export * from "./customers.types";
-export * from "./files.types";
+
