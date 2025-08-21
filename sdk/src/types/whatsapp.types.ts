ee1cd764cb759f4ad2a0a464f9ed4d33bb6e059d
juniomatias <jose.junio@bkopen.com>
2025-08-21 02:16:27 -0300

    feat : isForwarded in wppmessage

--- sdk/src/types/whatsapp.types.ts
+++ sdk/src/types/whatsapp.types.ts
@@ -36,6 +36,7 @@ export interface WppMessage {
 	body: string;
 	timestamp: string;
 	status: WppMessageStatus;
+	isForwarded?: boolean;
 	fileId?: number | null;
 	fileName?: string | null;
 	fileType?: string | null;
