933d6e2fee2947876d77bca9086143264d05e9cf
juniomatias <jose.junio@bkopen.com>
2025-10-12 15:15:26 -0300

    feat upload files

--- sdk/src/files.client.ts
+++ sdk/src/files.client.ts
@@ -47,49 +47,51 @@ class FilesClient extends ApiClient {
 	 * @param {UploadFileOptions} props - Opções para o upload do arquivo.
 	 * @returns {Promise<File>} Os dados do arquivo enviado.
 	 */
-	public async uploadFile(props: UploadFileOptions): Promise<File> {
-		const form = new FormData();
-		form.append("instance", props.instance);
-		form.append("dirType", String(props.dirType));
-		form.append("fileName", props.fileName);
+public async uploadFile(props: UploadFileOptions): Promise<File> {
+  const form = new FormData();
+  form.append("instance", props.instance);
+  form.append("dirType", String(props.dirType));
+  form.append("fileName", props.fileName);
 
-		const src: any = props.buffer as any;
+  const src: any = props.buffer as any;
+  let ab: ArrayBuffer;
 
-		let ab: ArrayBuffer;
+  if (src instanceof ArrayBuffer) {
+    ab = src;
+  } else if (typeof SharedArrayBuffer !== "undefined" && src instanceof SharedArrayBuffer) {
+    const view = new Uint8Array(src);
+    const copy = new Uint8Array(view.byteLength);
+    copy.set(view);
+    ab = copy.buffer;
+  } else if (ArrayBuffer.isView(src)) {
+    const view = src as ArrayBufferView;
+    const total = view.byteLength;
+    const offset = (view as any).byteOffset ?? 0;
+    const backing = (view as any).buffer as ArrayBuffer | SharedArrayBuffer;
 
-		if (src instanceof ArrayBuffer) {
-			ab = src;
-		} else if (typeof SharedArrayBuffer !== "undefined" && src instanceof SharedArrayBuffer) {
-			const view = new Uint8Array(src);
-			const copy = new Uint8Array(view.byteLength);
-			copy.set(view);
-			ab = copy.buffer;
-		} else if (ArrayBuffer.isView(src)) {
-			const view = src as ArrayBufferView;
-			const total = view.byteLength;
-			const offset = (view as any).byteOffset ?? 0;
-			const backing: ArrayBufferLike = (view as any).buffer;
+    if (typeof SharedArrayBuffer !== "undefined" && backing instanceof SharedArrayBuffer) {
+      const from = new Uint8Array(backing, offset, total);
+      const copy = new Uint8Array(total);
+      copy.set(from);
+      ab = copy.buffer;
+    } else {
+      ab = (backing as ArrayBuffer).slice(offset, offset + total);
+    }
+  } else {
+    const u8 = Uint8Array.from(src as ArrayLike<number>);
+    ab = u8.buffer;
+  }
 
-			if (typeof SharedArrayBuffer !== "undefined" && backing instanceof SharedArrayBuffer) {
-				const from = new Uint8Array(backing, offset, total);
-				const copy = new Uint8Array(total);
-				copy.set(from);
-				ab = copy.buffer;
-			} else {
-				ab = (backing as ArrayBuffer).slice(offset, offset + total);
-			}
-		} else {
-			const u8 = Uint8Array.from(src as ArrayLike<number>);
-			ab = u8.buffer;
-		}
+  const blob = new Blob([ab], { type: props.mimeType || "application/octet-stream" });
 
-		const blob = new Blob([ab], { type: props.mimeType || "application/octet-stream" });
+  form.append("file", blob, props.fileName);
 
-		form.append("file", blob, props.fileName);
+  const { data } = await this.ax.post<DataResponse<File>>("/api/files", form, {
+    headers: { "Content-Type": undefined },
+  });
 
-		const { data } = await this.ax.post<DataResponse<File>>("/api/files", form);
-		return data.data;
-	}
+  return data.data;
+}
 
 	/**
 	 * Deleta um arquivo pelo ID.
